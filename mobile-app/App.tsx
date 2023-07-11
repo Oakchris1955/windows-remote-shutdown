574eb0394aa7c49ecec96703b3de03b04743a95e
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2023-07-11 15:56:22 +0300

    Create authorization token definition menu

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { Modal, Pressable, StyleSheet, Text, TextInput, View, ViewProps } from 'react-native';
-import { ComponentProps, ReactNode, useState } from 'react';
+import { ComponentProps, ReactNode, useState, useEffect } from 'react';
 
 import SelectDropdown from 'react-native-select-dropdown'
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface DisplayedMethod {
     label: string,
@@ -59,8 +59,25 @@ const fieldStyles = StyleSheet.create({
 export default function App() {
     const [selectedMethod, setSelectedMethod] = useState(defaultMethod);
     const [optionsVisible, setOptionsVisibility] = useState(false);
+    const [tokenVisible, setTokenVisible] = useState(false);
     const [timeout, setTimeout] = useState(0);
     const [forcefulShutdown, setForcefulShutdown] = useState(true);
+
+    const [authToken, setAuthToken] = useState<string | null | undefined>(undefined);
+    const [userToken, setUserToken] = useState<string | undefined>(undefined);
+
+    useEffect(() => {
+        async function getAuthToken() {
+            if (typeof authToken !== "string") {
+                const storage_authToken = await AsyncStorage.getItem("authToken");
+                setAuthToken(storage_authToken);
+            } else {
+                AsyncStorage.setItem("authToken", authToken);
+            }
+        }
+
+        getAuthToken()
+    }, []);
 
 	return (
 		<View style={styles.container}>
@@ -81,12 +98,18 @@ export default function App() {
                 <Text style={{fontSize: 10}}>Περισσότερες επιλογές</Text>
             </Pressable>
             <Modal visible={optionsVisible}>
-                <Pressable
-                    style={{backgroundColor: "#eee", paddingVertical: "2%", paddingHorizontal: "4%", width: "30%"}}
-                    onPress={() => setOptionsVisibility(false)}
-                >
-                    <Text style={{width: "100%", textAlign: "center"}}>Επιστροφή</Text>
-                </Pressable>
+                <View style={{width: "100%", flexDirection: "row", justifyContent: "space-between"}}>
+                    <Pressable
+                        style={{backgroundColor: "#eee", paddingVertical: "2%", paddingHorizontal: "4%", width: "30%"}}
+                        onPress={() => setOptionsVisibility(false)}>
+                        <Text style={{width: "100%", flexGrow: 1, textAlign: "center", verticalAlign: "middle"}}>Επιστροφή</Text>
+                    </Pressable>
+                    <Pressable
+                        style={{backgroundColor: "#eee", paddingVertical: "2%", paddingHorizontal: "4%", width: "50%"}}
+                        onPress={() => setTokenVisible(true)}>
+                        <Text style={{width: "100%", flexGrow: 1, textAlign: "center", verticalAlign: "middle"}}>Ορισμός πιστοποιητικού εξουσιοδότησης</Text>
+                    </Pressable>
+                </View>
                 <View style={{justifyContent: "center", alignItems: "center", width: "100%", height: "100%"}}>
                     <View style={{backgroundColor: "#fff", padding: "1%", alignItems: "center", width: "100%"}}>
                         <Field label='Εκτέλεση ενέργειας σε ... δεύτερα:' style={styles.genericField}>
@@ -116,6 +139,20 @@ export default function App() {
                             />
                         </Field>
                     </View>
+                </View>
+            </Modal>
+            <Modal visible={authToken === null || tokenVisible}>
+                <View style={{justifyContent: "center", alignItems: "center", width: "100%", height: "100%"}}>
+                    <TextInput textAlign='center' placeholder='Εισάγετε το πιστοποιητικό εξουσιοδότησης' defaultValue={userToken} style={{backgroundColor: "#ddd", width: "90%", margin: "2%"}} onChangeText={setUserToken}/>
+                    <Pressable style={{backgroundColor: "#eee", padding: "1%"}} onPress={() => {
+                        if (typeof userToken === "string") {
+                            setAuthToken(userToken);
+                            setOptionsVisibility(false);
+                            setTokenVisible(false);
+                        }
+                    }}>
+                        <Text>Υποβολή πιστοποιητικού</Text>
+                    </Pressable>
                 </View>
             </Modal>
 			<StatusBar style="auto"/>
