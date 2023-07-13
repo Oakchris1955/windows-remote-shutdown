b7592edccef22de5a2097adbc40a6b04a2ec0cb9
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2023-07-13 17:33:32 +0300

    Fix app.config.ts
    Changes I had to make in order to get this working:
    1) Replace ES6 import statement with standard CommonJS require statement
    2) Place spread operators BEFORE dynamically defined fields, so that they wouldn't overwrite them

--- mobile-app/app.config.ts
+++ mobile-app/app.config.ts
@@ -1,19 +1,19 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
-import 'dotenv/config';
+require('dotenv').config();
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
+    ...config,
     android: {
-        package: process.env.ANDROID_PACKAGE,
-        ...config.android
+        ...config.android,
+        package: process.env.ANDROID_PACKAGE
     },
     extra: {
+        ...config.extra,
         eas: {
-            projectId: process.env.PROJECT_ID,
-            ...config.extra?.eas
-        },
-        ...config.extra
+            ...config.extra?.eas,
+            projectId: process.env.PROJECT_ID
+        }
     },
     name: config.name as string,
     slug: config.slug as string,
-    ...config
 });
